decd6f15ec45f483f527afd6a7fdddf9ea338951
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-13 23:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+  const ropeLengthsInput = document.getElementById('rope-lengths');
+  const resultDiv = document.getElementById('result');
+
+  const ropeLengths = ropeLengthsInput.value.split(',').map(Number);
+
+  if (ropeLengths.length < 2) {
+    resultDiv.textContent = "Please enter at least two rope lengths.";
+    return;
 }  
+
+
+  let ans = 
+	 resultDiv.innerText = findMinCostRopes(ropeLengths);;
+}
+
+function findMinCostRopes(arr) {
+    let minCost = 0;
+    let minHeap = [];
+    for (let i = 0; i < arr.length; i++) {
+      minHeap.push(arr[i]);
+      minHeap.sort((a, b) => a - b);
+    }
+
+    while (minHeap.length > 1) {
+      const first = minHeap.shift();
+      const second = minHeap.shift();
+      const cost = first + second;
+      minCost += cost;
+      minHeap.push(cost);
+      minHeap.sort((a, b) => a - b);
+    }
+
+   return minCost;
+  }
