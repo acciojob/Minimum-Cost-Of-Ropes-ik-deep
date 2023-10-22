555a8a2a3cf22058fc3059dddce0f462c0b20d9a
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-22 23:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,34 @@
- 
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
- 
- 
 function calculateMinCost() {
-  //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
-  }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
+  const ropeLengthsInput = document.getElementById('rope-lengths');
+  const resultDiv = document.getElementById('result');
+
+  const ropeLengths = ropeLengthsInput.value.split(',').map(Number);
+
+  if (ropeLengths.length < 2) {
+    resultDiv.textContent = "Please enter at least two rope lengths.";
+    return;
 }  
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
+function findMinCostRopes(arr) {
+    let minCost = 0;
+    let minHeap = [];
+
+    // Convert array to a min heap
+    for (let i = 0; i < arr.length; i++) {
+      minHeap.push(arr[i]);
+      minHeap.sort((a, b) => a - b);
+    }
+
+    while (minHeap.length > 1) {
+      const first = minHeap.shift();
+      const second = minHeap.shift();
+      const cost = first + second;
+      minCost += cost;
+      minHeap.push(cost);
+      minHeap.sort((a, b) => a - b);
+    }
+
+    resultDiv.textContent = `Minimum Cost of Ropes: ${minCost}`;
   }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
+
+  findMinCostRopes(ropeLengths);
 }
