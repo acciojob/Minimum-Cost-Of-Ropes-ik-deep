3e7c270d1144e2a8006348d3e2725b5297ff3f9c
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-24 09:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,121 @@
-function calculateMinCost() {
-  const ropeLengthsInput = document.getElementById('rope-lengths');
-  const resultDiv = document.getElementById('result');
-
-  const ropeLengths = ropeLengthsInput.value.split(',').map(Number);
-
-  if (ropeLengths.length < 2) {
-    resultDiv.textContent = "Please enter at least two rope lengths.";
-    return;
-}  
-function findMinCostRopes(arr) {
-    let minCost = 0;
-    let minHeap = [];
-
-    // Convert array to a min heap
-    for (let i = 0; i < arr.length; i++) {
-      minHeap.push(arr[i]);
-      minHeap.sort((a, b) => a - b);
-    }
-
-    while (minHeap.length > 1) {
-      const first = minHeap.shift();
-      const second = minHeap.shift();
-      const cost = first + second;
-      minCost += cost;
-      minHeap.push(cost);
-      minHeap.sort((a, b) => a - b);
-    }
-
-    resultDiv.textContent = `Minimum Cost of Ropes: ${minCost}`;
+class MinHeap {
+  constructor() {
+    this.heap = [];
   }
 
-  findMinCostRopes(ropeLengths);
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  extractMin() {
+    if (this.isEmpty()) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  heapifyUp() {
+    let currentIndex = this.heap.length - 1;
+
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+      if (this.heap[currentIndex] >= this.heap[parentIndex]) break;
+
+      [this.heap[currentIndex], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[currentIndex],
+      ];
+
+      currentIndex = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let currentIndex = 0;
+
+    while (true) {
+      const leftChildIndex = 2 * currentIndex + 1;
+      const rightChildIndex = 2 * currentIndex + 2;
+      let smallestIndex = currentIndex;
+
+      if (
+        leftChildIndex < this.heap.length &&
+        this.heap[leftChildIndex] < this.heap[smallestIndex]
+      ) {
+        smallestIndex = leftChildIndex;
+      }
+
+      if (
+        rightChildIndex < this.heap.length &&
+        this.heap[rightChildIndex] < this.heap[smallestIndex]
+      ) {
+        smallestIndex = rightChildIndex;
+      }
+
+      if (currentIndex === smallestIndex) break;
+
+      [this.heap[currentIndex], this.heap[smallestIndex]] = [
+        this.heap[smallestIndex],
+        this.heap[currentIndex],
+      ];
+
+      currentIndex = smallestIndex;
+    }
+  }
+}
+// Utility function to check if a variable is iterable
+function isIterable(obj) {
+  return obj != null && typeof obj[Symbol.iterator] === 'function';
+}
+
+// Example usage:
+const inputArray = [1, 2, 3, 4]; // Replace with your input data
+
+if (isIterable(inputArray)) {
+  const result = calculateMinCost(inputArray);
+  console.log("Result:", result);
+} else {
+  console.error("Input is not iterable. Please provide a valid iterable.");
+}
+
+function calculateMinCost(arr) {
+  if (!arr) return 0; // Handle the case when arr is falsy (e.g., undefined or null)
+  
+  // Convert non-iterable inputs to an array (assuming it's a single value)
+  if (!Array.isArray(arr)) {
+    arr = [arr];
+  }
+
+  // Create a priority queue (min-heap) to store rope lengths
+  const minHeap = new MinHeap();
+
+  // Insert all rope lengths into the min-heap
+  for (const rope of arr) {
+    minHeap.insert(rope);
+  }
+
+  let totalCost = 0;
+
+  // Connect ropes until there's only one rope left in the heap
+  while (minHeap.heap.length > 1) {
+    const rope1 = minHeap.extractMin();
+    const rope2 = minHeap.extractMin();
+
+    const combinedCost = rope1 + rope2;
+    totalCost += combinedCost;
+
+    // Insert the combined rope back into the min-heap
+    minHeap.insert(combinedCost);
+  }
+
+  return totalCost;
 }
